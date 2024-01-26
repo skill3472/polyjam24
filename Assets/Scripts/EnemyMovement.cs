ca032a83deda6a9c06b198666ebf4b7fcbd72a69
Maksymilian Tym <tymszczur@gmail.com>
2024-01-26 20:04:33 +0100

    Added enemy movement by @Jason
    ... and patched it a little

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+using UnityEditor.Rendering;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyMovement : MonoBehaviour
+{
+	[Min(0f)] public float speed = 5f;
+	public Transform startPosition, targetPosition;
+	
+	private Rigidbody2D rb2D;
+
+	private void Awake() => rb2D = GetComponent<Rigidbody2D>();
+
+	private void FixedUpdate()
+	{
+		if(NotReachedTarget())
+		{
+			MoveTowardsTarget();
+		} else {
+			Transform temp = startPosition;
+			startPosition = targetPosition;
+			targetPosition = temp;
+			MoveTowardsTarget();
+		}
+	}
+
+	private bool NotReachedTarget()
+	{
+		float distance = Vector2.Distance(gameObject.transform.position, targetPosition.position);
+
+		return distance > Mathf.Epsilon;
+	}
+
+	private void MoveTowardsTarget()
+	{
+		float step = speed*Time.fixedDeltaTime;
+		Vector2 direction = Vector2.MoveTowards(gameObject.transform.position, targetPosition.position, step);
+		
+		rb2D.MovePosition(direction);
+	}
+}
