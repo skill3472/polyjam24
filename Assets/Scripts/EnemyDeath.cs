01bcc5fc60e9afca48fa83d0ac14b9e06e056d85
Maksymilian Tym <tymszczur@gmail.com>
2024-01-27 08:35:19 +0100

    Added basic particles for enemy death

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
+    public GameObject particlePrefab;
     public void Die(){
         Debug.Log("Goodbye...");
+        GameObject x = Instantiate(particlePrefab, transform.position, transform.rotation);
+        Destroy(x, 3f);
         Destroy(gameObject);
     }
 }
